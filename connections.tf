8ffa52febdd962483986cef1148327639a6960fa
reji-vl <56560619+reji-vl@users.noreply.github.com>
2020-06-02 21:14:08 +0530

    Delete connections.tf

--- connections.tf
+++
@@ -1,6 +0,0 @@
-provider "azurerm" {
-	subscription_id = "ab833764-bdf6-4d19-af30-0af2a16fd669"
-	client_id = "a0e59edb-496d-445f-bf02-4bb3a9986cae"
-	client_secret = "92257f47-6469-4574-b08b-c4933df4f696"
-	tenant_id = "105b2061-b669-4b31-92ac-24d304d195dc"
-}
